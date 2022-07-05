1d6dada02e38ece8f4d498fb1ad43b1c443fb387
iamomoro <iamomoroit@gmail.com>
2022-07-05 18:01:28 +0300

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        leading: Icon(Icons.menu),
+        actions: [],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: '',
+            ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: '',
+            )
+        ],
+      ),
+      body: Column(children:
+      [
+        //Find the best coffee for you
+
+        //Search Bar
+
+        //horizon listview of coffee tiles
+      ]),
+    );
+  }
+}
