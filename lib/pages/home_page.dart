921b5e2672152fb51b22413c503a5c8857a4ca36
iamomoro <iamomoroit@gmail.com>
2022-07-05 20:03:22 +0300

    google-fonts-update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,6 @@
+import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
+
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,7 +17,12 @@ class _HomePageState extends State<HomePage> {
         elevation: 0,
         backgroundColor: Colors.transparent,
         leading: Icon(Icons.menu),
-        actions: [],
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: Icon(Icons.person),
+    ),
+             ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
@@ -36,6 +43,15 @@ class _HomePageState extends State<HomePage> {
       body: Column(children:
       [
         //Find the best coffee for you
+    Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Text(
+          "Find the best coffee for you",
+        style: GoogleFonts.bebasNeue(
+            fontSize: 56,
+          ),
+        ),
+      )
 
         //Search Bar
 
